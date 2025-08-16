a2ba637cc440ae686b2b45d4e435a6b1a4091b15
laptoppe <138631123+laptoppe@users.noreply.github.com>
2025-08-16 19:15:38 -0400

    Implementado mecánicas básicas de juego
    Implementadas las siguientes funciones:
    - pick_random(int); Retorna un numero aleatorio desde 0 al entero "int".
    - pick_letter(); Dado el array "letter_pool" de caracteres, selecciona un carácter aleatorio.
    - stack_rule(stack); Booleano, verifica si el string "stack" cumple las reglas de juego.
    - pick(r); Mueve el último carácter del string en la fila "r" al inicio del string en la mano de juego "hand" si la nueva combinación es válida.
    - set(r); Concatena el string en la mano de juego "hand" al string en la fila "r" si el último carácter de la fila "r" y el primer carácter de "hand" son una combinación válida.
    
    Añadidos botones de juego.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,72 @@
+const letter_pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
+
+function pick_random(int) {
+    return Math.floor(Math.random() * (int));
+} 
+// Based on example from www.w3schools.com js tutorials
+
+function pick_letter() {
+    console.log("Picked random letter from pool.");
+    return letter_pool[pick_random(letter_pool.length)];
+}
+
 function deal_letters() {
     for (let i = 0; i < 5; i++) {
-        document.getElementById(`row_${i+1}`).innerHTML += "a";
-        console.log(`Attempted to write to row ${i+1}.`)
+        document.getElementById(`row_${i+1}`).innerHTML += pick_letter();
+        console.log(`Attempted to write to row ${i+1}.`);
     }
 }
 
-// TODO: Make dealer deal random letters or characters from some wider set
-// TODO: Make playing functions (Picking up letters, setting down elsewhere)
-// (Needs to contain some rules)
-// Also probably design rules
+function stack_rule(stack) { 
+    console.log(`function stack_rule(${stack}) ran`);
+    console.log(`stack length: ${stack.length}`);
+    for (let i = 0; i < stack.length - 1; i++) {
+        console.log(`checking chars at ${i}, ${i+1}`);
+        if (stack.charCodeAt(i) > stack.charCodeAt(i+1)) {
+            console.log(`stack rule check failed; returning false`);
+            return false;
+        }
+    }
+    console.log(`stack rule check succesful; returning true`);
+    return true;
+}
+
+function pick(r) {
+    console.log(`function pick(${r}) ran`);
+    let row = document.getElementById(`row_${r}`).innerHTML;
+    let hand = document.getElementById(`hand`).innerHTML;
+    if (row == "") {
+        console.log(`row ${r} is empty, aborting function pick(${r})`);
+        return false;
+    }
+    if (!(stack_rule(row.at(-1) + hand))) {
+        console.log(`new hand does not meet chain rule, aborting function pick(${r})`);
+        return false;
+    }
+    document.getElementById(`hand`).innerHTML = row.at(-1) + hand;
+    document.getElementById(`row_${r}`).innerHTML = row.slice(0, row.length-1);
+    return true;
+}
+
+function set(r) {
+    console.log(`function set(${r}) ran`);
+    let row = document.getElementById(`row_${r}`).innerHTML;
+    let hand = document.getElementById(`hand`).innerHTML;
+
+    if ( row.length == 0 ) {
+        console.log(`row empty or username field, skipping rule check`);
+        document.getElementById(`row_${r}`).innerHTML = hand;
+        document.getElementById(`hand`).innerHTML = "";
+        return true;
+    }
+
+    if ( !(stack_rule(row.at(-1) + hand.at(0))) && r != 6 ) {
+        console.log(`placement does not meet chain rule, aborting function set(${r})`);
+        return false;
+    }
+
+    console.log(`set(${r}) command successful`);
+    document.getElementById(`row_${r}`).innerHTML += hand;
+    document.getElementById(`hand`).innerHTML = "";
+    return true;
+}
